9ba546ddf3af5be32eb436c533398b9b8bb05405
sourabh <sourabhpande43@gmail.comm>
2023-05-19 10:09:19 +0530

    first commit

---
+++ projectTrack.js
@@ -0,0 +1,65 @@
+/* 
+👋@heading@👋
+
+ 👩‍💻♦️ Do you know react is a library or framework  🤔??
+ =>Library, Major defference between two of them is that,it takes minimum efforts.so when I say something as a library framework it simply means full fladges framework when we build our apps/web it needs a lot of things it needs routing, you'll need different routes where you can go different pages migrating through over that and there are lot more component inside your app react come as a small "library".
+
+🎈@Followup@🎈...................
+
+👩‍💻🤔♦️ have you see images corousers,so what it is? is it libraray or framework🤔??
+ =>it's Javascript Library Major defference between two of them is that,it takes minimum efforts to just put into our code
+ it just take minimum efforts for a library to just put it inside our code.
+
+🤼Tool Using 🤼
+   - Google Chrome
+   - Vs Code 
+ 
+♦️🛕H.W read about "Emmet"👀
+
+👩‍💻♦️ As we saw react it is a Javascript libraray when we say it is a library it mean any other Library & you can inject this react into you'r code as many as times or bare minimum things. 
+👩‍💻♦️ React it is a global object/element/variable you can use anywhere.
+
+👩‍💻🤔♦️ how does browser get to know about this document.createElement("h1") what is "document" in this case?
+         => it's obivious it comes from browser Api's browser know what is document. browser has a js engine it is knows basically what is happning inside you'r browser & browser know about 
+         window.document; <= who gave us this functinality, it's obivious 'Js-engin'
+
+♦️🛕H.W read about "react cdn"👀
+
+ref: - 🖇️ https://legacy.reactjs.org/docs/cdn-links.html 
+go and inject it into 📂index.html 
+
+
+♦️🛕H.W read about "What is crossorigin"👀
+ -Web pages often make requests to load resources on other servers. Here is where CORS comes in. A cross-origin request is a request for a resource (e.g. style sheets, iframes, images, fonts, or scripts) from another domain
+ @what is CORS??
+ Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
+
+👩‍💻🤔♦️ what is shortest program of javascript ??
+     =>The shortest JS program is"empty file". Because even then, JS engine does a lot of things. As always, even in this case, it creates the GEC(general education curriculam) which has memory space and the execution context. JS engine creates something known as 'window'
+
+    🎈or in react what is shortest program?
+     =>just inject the code & add somm !"CDN" link into it. to ensure that just go in browser terminl and write  
+     -"React" -> enter"
+     -"ReactDom" -> enter"
+     -"React.createElement" -> enter"
+
+👩‍💻🤔♦️ why there is two script line uses react in CDN ??
+      => it just not limited for browser it's limited for mobile,tab,3d react also might be aware or not do you know react Native.
+
+
+      [@identyfire🤖[1st]
+👩‍💻🤔♦️ How to create <h1>Hellow,react<h/> and show to home page in react??
+       react gives us this api's which is know as react.createElement it expect three arguments here. 
+      1st - tags, 2nd - object, 3rd - content(inside h1)
+      task is inject it into "<div class="root"/>" need to tell the react root element there is the place where we need to inject code so react gives you a api know as ReactDom.    
+      
+👩‍💻🤔♦️ what is ReactDom??
+       because I'm going to modify my dom reading something form Dom that's is 2nd picture came into play "ReactDom" how do we modify in the wake "crateRoot"(this tell the react what is going to inject that it mean tell "root")
+
+
+
+   
+
+
+
+*/
